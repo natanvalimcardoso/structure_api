306345091b1d4105d5f0ad81f9f5b89bd5989bdb
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-08-10 10:55:52 -0300

    save

---
+++ lib/api_desafio.dart/lib/model/filme.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Filme {
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreids;
+  final String id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  Filme({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreids,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+// {
+// adult: false,
+// backdrop_path: "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg",
+// genre_ids: [
+// 28,
+// 12,
+// 878,
+// ],
+// id: "634649",
+// original_language: "en",
+// original_title: "Spider-Man: No Way Home",
+// overview: "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+// popularity: 6925.636,
+// poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+// release_date: "2021-12-15",
+// title: "Spider-Man: No Way Home",
+// video: false,
+// vote_average: 8.2,
+// vote_count: 11703,
+// },
