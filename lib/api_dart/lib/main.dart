7fa3ca3e7835b6ba9a8314e04624296d2084bcd1
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-08-10 09:59:13 -0300

    insert

--- lib/api_dart/lib/main.dart
+++ lib/api_dart/lib/main.dart
@@ -2,5 +2,7 @@ import 'controller/alunos_controller.dart';
 
 void main() {
     // AlunosController().findAll();
-    AlunosController().findById('2');
+    // AlunosController().findById('2');
+    // AlunosController().update('2', 'TESTE TESTE');
+    
   }
